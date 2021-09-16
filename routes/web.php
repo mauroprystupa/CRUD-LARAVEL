a61290bd4a06a22f8928c463afd724e0fe04b70e
mauroprystupa <mauroprystupa88@gmail.com>
2021-09-15 21:16:29 -0300

    13- creo listado de productos 14-creo boton de cargar sillones 15-creo el boton de borrar los sillones 16- hago la logica para los botones

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SilloneController;
+
+use App\Models\Sillone;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +23,20 @@ Route::get('/', HomeController::class);
 Route::get('sillones', [SilloneController::class, 'index'])->name('sillones.index');
 Route::get('sillones/crear', [SilloneController::class, 'crear'])->name('sillones.crear');
 Route::get('sillones/modificar', [SilloneController::class, 'modificar'])->name('sillones.modificar');
+
+
+Route::post('sillones', function(Request $request){
+    
+    $nuevosillon = new Sillone;
+    $nuevosillon->nombre = $request->input('nombre');
+    $nuevosillon->descripcion = $request->input('descripcion');
+    $nuevosillon->precio = $request->input('precio');
+    $nuevosillon->save();
+    return redirect()->route('sillones.index')->with('info',' SE CREO EL SILLON CON EXITO');
+})->name('sillones.guardar');
+
+Route::delete('sillones/{id}', function ($id) {
+    $sillon = Sillone::findOrFail($id);
+   $sillon->delete();
+   return redirect()->route('sillones.index')->with('info',' SE BORRO EL SILLON CON EXITO'); 
+})->name('sillones.borrar');
